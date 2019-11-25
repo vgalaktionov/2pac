d44556c360cd57a804d591e72465f382ef5735f3
Vadim <vadim.galaktionov@gmail.com>
2019-11-25 01:18:40 +0100

    add some tunes

--- src/namesake.ts
+++ src/namesake.ts
@@ -1,3 +1,9 @@
+import open from 'open';
+import chalk from 'chalk';
+
 export default function namesake() {
-    console.log('2pac namesake');
+    console.log('2pac namesake\n');
+
+    console.log(chalk.cyan('Playing Changes by 2Pac on YouTube, enjoy!'));
+    open('https://www.youtube.com/watch?v=eXvBjCO19QY');
 }
